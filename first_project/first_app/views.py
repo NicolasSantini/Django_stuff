92939eab8f8cdf7637aecc18d3b039cd6966bbdd
NicolasSantini <santini.nic@gmail.com>
2024-04-22 14:14:29 +0200

    creating index and help page using the include function

--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
 
+from django.shortcuts import render
+from django.http import HttpResponse
 # Create your views here.
+def index(request):
+    index_dict = {'insert_index': "this is the index page and there's an index"}
+    return render(request, 'first_app/index.html', context=index_dict)
+def help(request):
+    help_dict = {'insert_help': "this is the help page and there's an help"}
+    return render(request,'first_app/help.html',context=help_dict)
