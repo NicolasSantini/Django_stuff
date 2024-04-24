b3e2e6e4841fff829334389e234377374dd7e723
NicolasSantini <santini.nic@gmail.com>
2024-04-24 11:02:38 +0200

    implemented some extends with body-block and created a navigation bar with navbar

--- first_project/forms/urls.py
+++ first_project/forms/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from forms import views
 
+app_name = 'forms'
+
 urlpatterns = [
     path("", views.index, name="index"),
     path("index/", views.index, name="index"),
